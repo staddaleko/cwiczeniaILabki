cc17c911a22c594f02c1bf03e6ec5d2b5d0d58b5
staddaleko <staddaleko@gmail.com>
2014-07-04 14:11:43 +0200

    komplikujemy jeszcze sprawy! Ale wszystko w miarę jasne ;)

--- Powtorka/Program.cs
+++ Powtorka/Program.cs
@@ -18,7 +18,7 @@ namespace Powtorka
         }
 
     }
-    class Kolo:Figura
+    class Kolo : Figura
     {
         public new void Rysuj1()
         {
@@ -30,13 +30,25 @@ namespace Powtorka
         }
 
     }
-    class Kwadrat : Figura
+    class Czworokat : Figura
+    {
+        public new void Rysuj1()
+        {
+            Console.WriteLine("Tu czworokąt z Łodzi (new)");
+        }
+        public override void Rysuj2()
+        {
+            Console.WriteLine("Tu czworokąt z Wrocławia (override)");
+        }
+    }
+    class Kwadrat : Czworokat
     {
         public new void Rysuj1()
         {
             Console.WriteLine("Tu kwadrat z Łodzi (new)");
         }
-        public override void Rysuj2()
+        //public override void Rysuj2()
+        public new void Rysuj2()//zobaczmy co sie stanie jak zmienimy 'override' na 'new' //w wyniku wyswietli sie 'czworokat', a nie 'kwadrat'. Wyswietlamy to co jest w funkcji bazowej.
         {
             Console.WriteLine("Tu kwadrat z Wrocławia (override)");
         }
@@ -49,11 +61,12 @@ namespace Powtorka
             switch (s.ToLower())
             {
                 case "kolo": return new Kolo();
+                case "czworokat": return new Czworokat();
                 case "kwadrat": return new Kwadrat();
             }
             return new Figura();
         }
-        
+
         static void Main(string[] args)
         {
             Figura zmienna;//gdyby była zmienna typu 'Kolo', wtedy by wyswietlalo "kolo"
@@ -63,6 +76,12 @@ namespace Powtorka
             zmienna = Utworz(s);
             zmienna.Rysuj1();
             zmienna.Rysuj2();
+
+            if (zmienna is Kwadrat)
+            {
+                ((Kwadrat)zmienna).Rysuj2();//upewniamy sie, że mimo wszystko gdy bedziemy chcieli "kwadrat" to wypisze nam 'kwadrat' zamiast 'czworokat'
+            }
+
             Console.ReadKey();
 
         }
