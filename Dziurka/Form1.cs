1b35e57199aabcb5531af4cfc99498ac8f86a3c6
staddaleko <staddaleko@gmail.com>
2014-07-04 17:43:10 +0200

    okienko z dziurką! yay!

---
+++ Dziurka/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+
+namespace Dziurka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath sciezka = new GraphicsPath();
+            sciezka.AddEllipse(0, 0, this.Width, this.Height);
+        }
+    }
+}
