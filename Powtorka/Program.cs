8452cdb0c7ce847b5ab67d5f0747b5cd5513594b
staddaleko <staddaleko@gmail.com>
2014-07-04 13:41:12 +0200

    powtórka dzisiaj

---
+++ Powtorka/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powtorka
+{
+    class Figura
+    {
+        public void Rysuj1()
+        {
+            Console.WriteLine("Tu Figura");
+        }
+        public virtual void Rysuj2()
+        {
+            Console.WriteLine("Tu Figura");
+        }
+
+    }
+    class Kolo:Figura
+    {
+        public override void Rysuj2()
+        {
+            Console.WriteLine("Tu koło z Wrocławia");
+        }
+
+        public new void Rysuj1()
+        {
+            Console.WriteLine("Tu koło z Łodzi");
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura zmienna;
+
+        }
+    }
+}
